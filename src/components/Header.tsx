6e5201acbaad16371e3713668c8cfe05acbbbbf1
Maryiamreda <maryiamreda@gmail.com>
2025-02-28 00:12:23 +0200

    project initialization

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Exclusive</h1>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Header;
