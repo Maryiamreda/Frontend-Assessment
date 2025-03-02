c8a3cce05048f1face3ad8b3b406c932cda14510
Maryiamreda <maryiamreda@gmail.com>
2025-03-02 19:15:45 +0200

    scroll to component

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -5,7 +5,7 @@ import Secure from '../assets/icons/Secure.svg'
 
 const Service = () => {
     return (
-        <div className=" mx-28 mt-20 pb-20 border-b  border-b-gray-300 justify-center items-center flex-col  flex  md:flex-row gap-20">
+        <div className=" mx-28 mt-20 pb-20  justify-center items-center flex-col  flex  md:flex-row gap-20">
             <div className='flex flex-col items-center gap-2'>
                 <div className='bg-black border-9 border-[#D1D4DB] rounded-full flex items-center justify-center w-15 h-15'>
                     <img src={ServiceIcon} className='w-7' />
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -7,7 +7,7 @@ import Ourproducts from '../components/Ourproducts';
 import Quicksearch from '../components/Quicksearch';
 import Secondheader from '../components/Secondheader';
 import Service from '../components/Service';
-
+import Arrow from '../assets/icons/Arrowright.svg';
 const Homepage = () => {
     return (
         <div className='  '>
@@ -23,6 +23,19 @@ const Homepage = () => {
             <Ourproducts />
             <Newarrivals />
             <Service />
+            <div className='mx-28 flex flex-row-reverse pt-10'>
+                <div className='bg-[#F7F7FC] p-2 rounded-full'
+                    onClick={() => {
+                        scrollTo({
+                            top: 0,
+                            behavior: 'smooth'
+                        });
+                    }}
+                >
+                    <img src={Arrow} className='-rotate-90 cursor-pointer' />
+
+                </div>
+            </div>
         </div>
     );
 }
